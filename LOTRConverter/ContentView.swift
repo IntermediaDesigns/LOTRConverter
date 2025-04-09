88983aadd989bb7354f9624ed928753b9d37bf47
Lynjai Jimenez <intermediadesignsllc@gmail.com>
2025-04-09 12:53:52 -0400

    Updated code for info button

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -8,97 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
-    
-    let showExchangeInfo = false
-    
-    
+    @State var showExchangeInfo = false
+
     var body: some View {
         ZStack {
-            // Background image and fill entire screen
+            // Background
             Image("background")
                 .resizable()
                 .scaledToFill()
                 .ignoresSafeArea()
-                
-            // Main content
-            VStack {
 
-                // Prancing pony image
-                Image(.prancingpony)
+            // Top Section
+            VStack {
+                Image("prancingpony")
                     .resizable()
                     .scaledToFit()
-                    .frame(height: 200)
+                    .frame(width: 200, height: 200)
 
-                // Currency exchange text
                 Text("Currency Exchange")
                     .font(.largeTitle)
                     .foregroundStyle(.white)
 
-                // Conversion section
                 HStack {
-                    // Left conversion section
                     VStack {
-                        // Currency
                         HStack {
-                            // Currency icon
-                            Image(.gold)
+                            Image("gold")
                                 .resizable()
                                 .scaledToFit()
-                                .frame(height: 50)
+                                .frame(height: 33)
 
-                            // Currency text
                             Text("Gold")
                                 .font(.headline)
                                 .foregroundStyle(.white)
                         }
-                        // Text field
-                        Text("This is text")
-                            
+                        Text("This is text.")
                     }
 
-                    // Equal sign
                     Image(systemName: "equal")
                         .font(.largeTitle)
                         .foregroundStyle(.white)
                         .symbolEffect(.pulse)
 
-                    // Right conversion section
                     VStack {
-                        // Currency
                         HStack {
-                            // Currency text
                             Text("Silver")
                                 .font(.headline)
                                 .foregroundStyle(.white)
 
-                            // Currency icon
-                            Image(.silver)
+                            Image("silver")
                                 .resizable()
                                 .scaledToFit()
-                                .frame(height: 50)
+                                .frame(height: 33)
                         }
-                        // Text field
-                        Text("This is text")
+                        Text("This is text.")
                     }
-
-                }
+                } // End of Top Section
                 
-                Spacer()
-                    
 
-                // Info button
-                Button {
-                    // Action for info button
-                    
-                } label: {
-                    Image(systemName: "info.circle.fill")
-                        .font(.largeTitle)
-                        .foregroundStyle(.white)
-                        .symbolEffect(.pulse)
-                        .shadow(color: .black.opacity(0.7), radius: 10, x: 0, y: 0)
-                }
-            }
-        }
+                Spacer() // Spacer between top and bottom sections
+                
+                // Bottom Section
+                
+                // Info Button
+                HStack {
+                    Spacer()
+                    Button {
+                        showExchangeInfo.toggle()
+                    } label: {
+                        Image(systemName: "info.circle.fill")
+                            .font(.largeTitle)
+                            .foregroundStyle(.white)
+                    }
+                    .padding(.trailing)
+                }  // End of Bottom Section
+
+                
+            } // Bottom of first VStack
+            
+
+            .border(.blue)  // Test edges
+            .frame(width: UIScreen.main.bounds.width)
+            
+        } // End of ZStack
     }
 }
 
